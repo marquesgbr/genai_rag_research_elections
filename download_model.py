6ae68680df4551e169643b381c58c3cf1d858efb
marquesgbr <gma5@cin.ufpe.br>
2026-09-22 22:25:34 -0300

    feat: add local embedding model download

---
+++ download_model.py
@@ -0,0 +1,15 @@
+# Download embeddingmodel from Hugging Face and save it physically to a local folder for later use in the project.
+
+from sentence_transformers import SentenceTransformer
+from config_env import EMBEDDING_MODEL_NAME, EMBEDDING_MODEL_LOCAL_PATH
+
+def download_and_save():
+    print("Downloading and loading the model from Hugging Face...")
+    model = SentenceTransformer(EMBEDDING_MODEL_NAME)
+
+    print("Saving the model physically to the local folder...")
+    model.save(EMBEDDING_MODEL_LOCAL_PATH)
+    print(f"Model saved successfully to {EMBEDDING_MODEL_LOCAL_PATH}")
+
+if __name__ == "__main__":
+    download_and_save()
