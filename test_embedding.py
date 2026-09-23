346a94f9820d04ea3ec30db4fa94577742e4b332
marquesgbr <gma5@cin.ufpe.br>
2026-09-22 22:27:37 -0300

    feat: add embedding coverage

---
+++ test_embedding.py
@@ -0,0 +1,25 @@
+from sentence_transformers import SentenceTransformer
+from config_env import EMBEDDING_MODEL_LOCAL_PATH
+
+# 1. Load a pretrained Sentence Transformer model
+model = SentenceTransformer(EMBEDDING_MODEL_LOCAL_PATH) 
+
+# The sentences to encode
+sentences = [
+    "O clima está ótimo hoje.",
+    "It's so sunny outside!",
+    "Ele dirigiu até o estádio",
+]
+
+# 2. Calculate embeddings by calling model.encode()
+embeddings = model.encode(sentences)
+print(embeddings.shape)
+# [3, 384]
+
+# 3. Calculate the embedding similarities
+similarities = model.similarity(embeddings, embeddings)
+print(similarities)
+
+# tensor([[1.0000, 0.6768, 0.1268],
+#         [0.6768, 1.0000, 0.2386],
+#         [0.1268, 0.2386, 1.0000]])
